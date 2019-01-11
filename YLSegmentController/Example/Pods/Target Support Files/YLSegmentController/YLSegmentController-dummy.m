3b27d57bf3d98b2634789b1b1feaa3fb6c4f6fd3
NSObject_Y <yangsui92@163.com>
2019-01-11 17:01:37 +0800

    0.1.0

---
+++ YLSegmentController/Example/Pods/Target Support Files/YLSegmentController/YLSegmentController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YLSegmentController : NSObject
+@end
+@implementation PodsDummy_YLSegmentController
+@end
---
+++ YLSegmentController/Example/YLSegmentController/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  YLSegmentController
+//
+//  Created by yangsui92@163.com on 01/11/2019.
+//  Copyright (c) 2019 yangsui92@163.com. All rights reserved.
+//
+
+import UIKit
+import YLSegmentController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let titles = ["推荐","获取","无语"];
+        let segmentPageNav = SegmentSliderController(HHMade: self, titles: titles)
+        segmentPageNav?.delegate = self
+        segmentPageNav?.childsViewControllers = [ThreeViewController(),SecViewController(),FirstViewController()]
+        self.view.addSubview(segmentPageNav!)
+        print("噶你")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController:SegementSliderDetegate{
+    func segmentController(_ controller: SegmentSliderController, selectIndex index: Int) {
+        
+    }
+    
+    func selectImageClick() {
+        print("搜索------")
+    }
+}
+
